39a130f8dea46f8fa367b0dd640d1a528bc7b119
r17x <ri7nz@evilfactory.id>
2025-10-30 00:56:37 +0700

    chore(flake): use flake-parts

--- flake.nix
+++ flake.nix
@@ -2,24 +2,21 @@
   description = "Tapak";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nix-filter.url = "github:numtide/nix-filter";
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nix-ocaml/nix-overlays";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
-        });
-      in
-      rec {
-        packages = pkgs.callPackage ./nix/default.nix { nix-filter = nix-filter.lib; };
-        defaultPackage = packages.tapak;
-        devShells = {
-          default = pkgs.callPackage ./nix/shell.nix { inherit packages; };
-        };
-      }
-    );
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = [ ./nix ];
+    };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -1,22 +1,22 @@
-{ lib, stdenv, ocamlPackages, nix-filter, doCheck ? false, pkgs }:
-
-with ocamlPackages;
+{ inputs, lib, ... }:
 
 let
-  genSrc = { dirs, files }:
+  genSrc =
+    { dirs, files }:
     let
       root = ./..;
 
-      mkDirMatcher = dirs: args:
+      mkDirMatcher =
+        dirs: args:
         let
           rootPath = toString args.root;
-          getParents = path:
+          getParents =
+            path:
             let
               parts = lib.filter (p: p != "") (lib.splitString "/" path);
               numParts = builtins.length parts;
-              mkPaths = n:
-                if n == 0 then []
-                else [ (lib.concatStringsSep "/" (lib.take n parts)) ] ++ (mkPaths (n - 1));
+              mkPaths =
+                n: if n == 0 then [ ] else [ (lib.concatStringsSep "/" (lib.take n parts)) ] ++ (mkPaths (n - 1));
             in
             mkPaths (numParts - 1);
 
@@ -25,108 +25,152 @@ let
 
         in
         path: type:
-          let
-            pathStr = toString path;
-            relPath = lib.removePrefix (rootPath + "/") pathStr;
-          in
-          builtins.any (dir:
-            relPath == dir || lib.hasPrefix (dir + "/") relPath
-          ) fullDirs
-          || (type == "directory" && builtins.elem relPath parentDirs);
+        let
+          pathStr = toString path;
+          relPath = lib.removePrefix (rootPath + "/") pathStr;
+        in
+        builtins.any (dir: relPath == dir || lib.hasPrefix (dir + "/") relPath) fullDirs
+        || (type == "directory" && builtins.elem relPath parentDirs);
     in
-    nix-filter.filter {
+    inputs.nix-filter.lib.filter {
       inherit root;
       include = [ "dune-project" ] ++ files ++ [ (mkDirMatcher dirs) ];
     };
-  buildTapak = args: buildDunePackage ({
-    version = "0.1.0";
-    doCheck = doCheck;
-    duneVersion = "3";
-    nativeBuildInputs = [ pkgs.pkg-config ];
-    buildInputs = [ pkgs.openssl ];
-    checkInputs = [ alcotest ];
-    PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
-      pkgs.openssl
-    ];
-  } // args);
 in
 
-rec {
-  simdutf =
+{
+
+  perSystem =
+    {
+      pkgs,
+      inputs',
+      self',
+      ...
+    }:
     let
-      simdutf-h = pkgs.fetchurl {
-        url = "https://github.com/simdutf/simdutf/releases/download/v7.5.0/simdutf.h";
-        sha256 = "sha256-Sh4ktRu6wfLcgnShdXdaxPB9E83IjkINzsibUqFrFD8=";
-      };
-      simdutf-cpp = pkgs.fetchurl {
-        url = "https://github.com/simdutf/simdutf/releases/download/v7.5.0/simdutf.cpp";
-        sha256 = "sha256-9GNP6/19MkljZHDQY5PWZeAd4X3S8pgyq+MjQ8sCU/k=";
-      };
+      buildTapak =
+        args:
+        pkgs.ocamlPackages.buildDunePackage (
+          {
+            version = "0.1.0";
+            doCheck = false;
+            duneVersion = "3";
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+            checkInputs = [ pkgs.ocamlPackages.alcotest ];
+            PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
+              pkgs.openssl
+            ];
+          }
+          // args
+        );
+
     in
-    buildTapak {
-      pname = "simdutf";
-      src = genSrc {
-        dirs = [ "pkg/simdutf" ];
-        files = [ "simdutf.opam" ];
+    {
+      _module.args.pkgs = inputs'.nixpkgs.legacyPackages.extend (
+        _self: super: {
+          ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
+        }
+      );
+      packages =
+        with pkgs.ocamlPackages;
+        with self'.packages;
+        {
+          simdutf =
+            let
+              simdutf-h = pkgs.fetchurl {
+                url = "https://github.com/simdutf/simdutf/releases/download/v7.5.0/simdutf.h";
+                sha256 = "sha256-Sh4ktRu6wfLcgnShdXdaxPB9E83IjkINzsibUqFrFD8=";
+              };
+              simdutf-cpp = pkgs.fetchurl {
+                url = "https://github.com/simdutf/simdutf/releases/download/v7.5.0/simdutf.cpp";
+                sha256 = "sha256-9GNP6/19MkljZHDQY5PWZeAd4X3S8pgyq+MjQ8sCU/k=";
+              };
+            in
+            buildTapak {
+              pname = "simdutf";
+              src = genSrc {
+                dirs = [ "pkg/simdutf" ];
+                files = [ "simdutf.opam" ];
+              };
+              DOWNLOAD_SIMDUTF = "false";
+              preBuild = ''
+                cp ${simdutf-h} pkg/simdutf/c/simdutf.h
+                cp ${simdutf-cpp} pkg/simdutf/c/simdutf.cpp
+                chmod +w pkg/simdutf/c/simdutf.h pkg/simdutf/c/simdutf.cpp
+              '';
+              buildInputs = [ dune-configurator ];
+              propagatedBuildInputs = [ ctypes ];
+            };
+
+          tapak = buildTapak {
+            pname = "tapak";
+            src = genSrc {
+              dirs = [
+                "src"
+                "pkg/kernel"
+              ];
+              files = [ "tapak.opam" ];
+            };
+            propagatedBuildInputs = [
+              eio
+              angstrom
+              hmap
+              logs
+              piaf
+              uri
+              ptime
+              yojson
+              mirage-crypto
+              mirage-crypto-rng
+            ];
+          };
+
+          tapak-compressions = buildTapak {
+            pname = "tapak-compressions";
+            src = genSrc {
+              dirs = [
+                "pkg/compressions"
+                "pkg/kernel"
+              ];
+              files = [ "tapak-compressions.opam" ];
+            };
+            buildInputs = [
+              dune-configurator
+              pkgs.brotli
+              pkgs.zstd
+            ];
+            PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
+              pkgs.brotli
+              pkgs.zstd
+            ];
+            propagatedBuildInputs = [
+              tapak
+              piaf
+              camlzip
+            ];
+          };
+
+          tapak-ppx = buildTapak {
+            pname = "tapak-ppx";
+            src = genSrc {
+              dirs = [ "pkg/ppx" ];
+              files = [ "tapak-ppx.opam" ];
+            };
+            nativeBuildInputs = [
+              alcotest
+              ocamlformat
+            ];
+            propagatedBuildInputs = [
+              tapak
+              ppxlib
+            ];
+          };
+
+        };
+      # defaultPackage = self'.packages.tapak;
+      devShells = {
+        default = pkgs.callPackage ./shell.nix { packages = self'.packages; };
       };
-      DOWNLOAD_SIMDUTF = "false";
-      preBuild = ''
-        cp ${simdutf-h} pkg/simdutf/c/simdutf.h
-        cp ${simdutf-cpp} pkg/simdutf/c/simdutf.cpp
-        chmod +w pkg/simdutf/c/simdutf.h pkg/simdutf/c/simdutf.cpp
-      '';
-      buildInputs = [ dune-configurator ];
-      propagatedBuildInputs = [ ctypes ];
     };
-
-  tapak = buildTapak {
-    pname = "tapak";
-    src = genSrc {
-      dirs = [ "src" "pkg/kernel" ];
-      files = [ "tapak.opam" ];
-    };
-    propagatedBuildInputs = [
-      eio
-      angstrom
-      hmap
-      logs
-      piaf
-      uri
-      ptime
-      yojson
-      mirage-crypto
-      mirage-crypto-rng
-    ];
-  };
-
-  tapak-compressions = buildTapak {
-    pname = "tapak-compressions";
-    src = genSrc {
-      dirs = [ "pkg/compressions" "pkg/kernel" ];
-      files = [ "tapak-compressions.opam" ];
-    };
-    buildInputs = [ dune-configurator pkgs.brotli pkgs.zstd ];
-    PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
-      pkgs.brotli
-      pkgs.zstd
-    ];
-    propagatedBuildInputs = [
-      tapak
-      piaf
-      camlzip
-    ];
-  };
-
-  tapak-ppx = buildTapak {
-    pname = "tapak-ppx";
-    src = genSrc {
-      dirs = [ "pkg/ppx" ];
-      files = [ "tapak-ppx.opam" ];
-    };
-    nativeBuildInputs = [ alcotest ocamlformat ];
-    propagatedBuildInputs = [
-      tapak
-      ppxlib
-    ];
-  };
 }
