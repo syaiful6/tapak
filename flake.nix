29ace75f7456b920bac48020b6cc8514da42a5ab
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-12-14 22:37:24 +0700

    flake: refactor
    Co-authored-by: toastal <toastal@posteo.net>

--- flake.nix
+++ flake.nix
@@ -2,24 +2,55 @@
   description = "Tapak";
 
   inputs = {
-    nix-filter.url = "github:numtide/nix-filter";
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
-        });
-      in
-      rec {
-        packages = pkgs.callPackage ./nix/default.nix { nix-filter = nix-filter.lib; };
-        defaultPackage = packages.tapak;
-        devShells = {
-          default = pkgs.callPackage ./nix/shell.nix { inherit packages; };
-        };
-      }
-    );
+  outputs =
+    { self, nixpkgs }:
+    let
+      # There is also pkgs.ocaml.meta.platforms
+      # flakeExposed is a subset of that.
+      allSystems = nixpkgs.lib.systems.flakeExposed;
+      # withPkgs: ({ Nixpkgs, System } -> AttrSet) -> PerSystemAttrSet
+      withPkgs =
+        pkgsCallback:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                (import ./nix/overlays)
+              ];
+            };
+          in
+          pkgsCallback { inherit pkgs system; }
+        );
+    in
+    {
+      packages = withPkgs (
+        { pkgs, system }:
+        {
+          default = self.packages.${system}.tapak;
+
+          inherit (pkgs.ocamlPackages)
+            tapak
+            tapak-compressions
+            tapak-ppx
+            simdutf
+            ;
+        }
+      );
+
+      devShells = withPkgs (
+        { pkgs, ... }:
+        {
+          default = pkgs.tapak.dev-shell;
+        }
+      );
+
+      overlays.default = import ./nix/overlays;
+
+      formatter = withPkgs ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
+    };
 }
